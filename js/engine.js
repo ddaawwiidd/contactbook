8c3f2a67e4de6ea3bc2d698f881c8c8d3489ce08
Dawid Jkw <djankowski@software.altkom.pl>
2016-12-21 14:39:20 +0100

    js cleaned up

--- js/engine.js
+++ js/engine.js
@@ -5,11 +5,10 @@ var Contacts = {
 	init: function(){
 		if(localStorage.getItem("contact") !==null){
 			this.contacts = (JSON.parse(localStorage.getItem("contact")));
-		console.log(this.contacts);
 		};
 	},
 	
-	//stores data from the form
+	//takes data from the form
 	takeContact: function(){
 		var entry = {
 			name: document.getElementById("nameInput").value,
@@ -17,17 +16,16 @@ var Contacts = {
 			email: document.getElementById("emailInput").value,
 			phone: document.getElementById("phoneInput").value
 		};
-		console.log(entry);
 		this.contacts.push(entry);
-		console.log(this.contacts);
 		Contacts.storeAdd();
 	},
 	
-	
+	//stores data in local storage
 	storeAdd: function(){
 		localStorage.setItem("contact",JSON.stringify(this.contacts));
 	},
-		
+	
+	//searches for matching contacts
 	search: function(){
 		var parameter = {
 			name: document.getElementById("nameInput").value,
@@ -38,44 +36,40 @@ var Contacts = {
 		var print = "";
 		for (var i = 0; i < this.contacts.length; i++){
 			if(this.contacts[i].name == parameter.name || this.contacts[i].surname == parameter.surname || this.contacts[i].email == parameter.email || this.contacts[i].phone == parameter.phone ){
-				console.log(this.contacts[i]);
 				print += "<tr><td>"+this.contacts[i].name + "</td><td>" + this.contacts[i].surname + "</td><td>" + this.contacts[i].email + "</td><td>" + this.contacts[i].phone + "</td><td> <input class='button-primary' type='button' onclick='Contacts.editRecord(\""+i+"\")' value='Edit'> <input class='button' type='button' value='Remove' onclick='Contacts.removeRecord(\""+i+"\")'>" +"</td></tr>";
 			} else if (parameter.name == "" && parameter.surname == "" && parameter.email == "" && parameter.phone == ""){
-				console.log(this.contacts[i]);
 				print += "<tr><td>"+this.contacts[i].name + "</td><td>" + this.contacts[i].surname + "</td><td>" + this.contacts[i].email + "</td><td>" + this.contacts[i].phone + "</td><td> <input class='button-primary' type='button' onclick='Contacts.editRecord(\""+i+"\")' value='Edit'> <input class='button' type='button' value='Remove' onclick='Contacts.removeRecord(\""+i+"\")'>" +"</td></tr>";
 			};
 		};
 		document.getElementById("result").innerHTML = print;
 	},
 	
+	//edits contact
 	editRecord: function(record){
 		document.getElementById("nameInput").value = this.contacts[record].name;
 		document.getElementById("surnameInput").value = this.contacts[record].surname;
 		document.getElementById("emailInput").value = this.contacts[record].email;
 		document.getElementById("phoneInput").value = this.contacts[record].phone;
 		var index = this.contacts.indexOf(this.contacts[record]);
-		console.log(index);
 		if(index > -1){
 			this.contacts.splice(index,1);
 		};
-		console.log(this.contacts);
 		Contacts.search();
 	},
 	
+	//deletes local storage
 	storeRemove: function(){
 		localStorage.clear();
 		location.reload();
 	},
 	
+	//deletes single contact
 	removeRecord: function(record){
-		console.log(record);
 		var index = this.contacts.indexOf(this.contacts[record]);
-		console.log(index);
 		if(index > -1){
 			this.contacts.splice(index,1);
 		};
 		Contacts.search();
-		console.log(this.contacts);
 		Contacts.storeAdd();
 	}
 	
